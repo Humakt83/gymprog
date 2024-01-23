3624b1dd80bab6541b9947cb9bf101bc0741782d
Humakt83 <urlact@yahoo.com>
2024-01-23 23:40:34 +0200

    Update about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,26 +1,36 @@
 <svelte:head>
-	<title>About</title>
+	<title>Muuta</title>
 	<meta name="description" content="About this app" />
 </svelte:head>
 
-<div class="text-column">
-	<h1>About this app</h1>
+<section>
+	<h1>Muuta</h1>
 
-	<p>
-		This is a <a href="https://kit.svelte.dev">SvelteKit</a> app. You can make your own by typing the
-		following into your command line and following the prompts:
-	</p>
+	<p>Saitti saliohjelmilleni. Tehty käyttäen <a href="https://kit.svelte.dev">SvelteKit</a>.</p>
 
-	<pre>npm create svelte@latest</pre>
+	<h2>Saliaiheisia linkkejä</h2>
 
-	<p>
-		The page you're looking at is purely static HTML, with no client-side interactivity needed.
-		Because of that, we don't need to load any JavaScript. Try viewing the page's source, or opening
-		the devtools network panel and reloading.
-	</p>
+	<div class="links">
+		<a href="https://www.youtube.com/@athleanx">ATHLEAN-X</a>
 
-	<p>
-		The <a href="/sverdle">Sverdle</a> page illustrates SvelteKit's data loading and form handling. Try
-		using it with JavaScript disabled!
-	</p>
-</div>
+		<a href="https://www.treeniohjelma.org/liikepankki/">Liikepankki</a>
+	</div>
+
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		flex: 0.6;
+	}
+
+	.links {
+		display: flex;
+		flex-direction: column;
+		row-gap: 0.4rem;
+	}
+
+
+</style>
